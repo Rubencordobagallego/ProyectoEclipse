49c956b1b93d8c99000e382c66dab38e91a6bcba
Ruben Cordoba <rcorgal0509@g.educaand.es>
2024-10-30 09:28:09 +0100

    otros cambios

--- src/es/iesoretania/proyectoeclipse/datos/Prueba.java
+++ src/es/iesoretania/proyectoeclipse/datos/Prueba.java
@@ -4,7 +4,24 @@ public class Prueba {
 
 	public static void main(String[] args) {
 			System.out.println("Hola Mundo!!");
+			
+			System.out.println("Hola Mundo!!");
 
+			System.out.println("Hola Mundo!!");
+
+			System.out.println("Hola Mundo!!");
+
+			System.out.println("Hola Mundo!!");
+			
+			System.out.println("Hola yo soy mateo hola hola");
+			
+			System.out.println("Hola Mundo!!");
+			
+			System.out.println("Hola yo guapo hola hola hola");
+			
+			
+
+			
 	}
 
-}
+} 
--- src/es/iesoretania/proyectoeclipse/datos/Prueba2.java
+++ src/es/iesoretania/proyectoeclipse/datos/Prueba2.java
@@ -1,5 +1,21 @@
 package es.iesoretania.proyectoeclipse.datos;
 
-public class Prueba2 {
+public class Prueba2 { public static void main(String[] args) {
+	System.out.println("Hola Mundo!!");
+	
+	System.out.println("Hola Mundo!!");
+
+	System.out.println("Hola Mundo!!");
+
+	System.out.println("Hola Mundo!!");
+
+	System.out.println("Hola Mundo!!");
+
+	System.out.println("Hola Mundo!!");
+	
+	System.out.println("Hola Mundo!!");
+
+	
+}
 
 }
